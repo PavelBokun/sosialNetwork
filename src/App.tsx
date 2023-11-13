06932447487b2d33841421d6358ba03bb99305ef
PavelBokun <pavel.snabteh@gmail.com>
2023-11-13 19:55:28 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='app-wrapper' >
+    <Header />
+     <Footer />
+     <p>hi</p>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = ()=>{
+    return(
+        <div>
+           <header className="header">
+            <img src="https://i.pinimg.com/originals/c4/29/70/c42970c030554b230b644cc6078f2898.jpg" alt="gg" />
+           </header>
+           <nav>
+
+           </nav>
+
+        </div>
+    )
+}
+
+export  default Header
