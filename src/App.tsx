670e200e2f39d0b33411ffa1518ffe13cb49f42c
PavelBokun <pavel.snabteh@gmail.com>
2023-11-15 20:51:03 +0300

    lesson18

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import "./App.css";
 import Header from "./component/Header/Header";
 import NavBar from "./component/Navbar/NavBar";
+import Dialogs from "./component/Dialogs/Dialogs";
 import Profile from "./component/Profile/Profile";
-import MyPosts from "./component/Profile/My posts/MyPosts";
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <NavBar />
+      {/* <Profile /> */}
+      <div className="app-wrapper-content">
+      {/* <Dialogs /> */}
       <Profile />
+      </div>
+      
     </div>
   );
 }
